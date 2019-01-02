43236a810d1988965bc524eff1e7d721c9e63165
Igor Dominiak <dominiak.igor@gmail.com>
2019-01-02 14:02:56 +0100

    Add fixture helper for tests

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Yatapp.MixProject do
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
@@ -58,4 +59,7 @@ defmodule Yatapp.MixProject do
       links: %{"GitHub" => "https://github.com/LLInformatics/yatapp-elixir"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
